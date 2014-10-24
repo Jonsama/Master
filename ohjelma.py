167ce16463bbd06ce518bfceabbc5d7fabb697f0
K0ntrol <codgizmo@hotmail.com>
2014-10-24 14:25:22 +0300

    Added source code
    First commit ever

---
+++ ohjelma.py
@@ -0,0 +1,18 @@
+a = 3
+b = 5
+
+print("A:n arvo on", a, "ja B:n arvo on", b)
+
+a = a * 3
+b = b + 10
+
+print("A:n arvo on", a, "ja B:n arvo on", b)
+
+if a > b:
+	print("Game over")
+elif a > 1000:
+	print("Game over")
+elif b > 1000:
+	print("Game over")
+
+print("Heippa!")
