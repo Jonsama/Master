8d7ce71b4b31cd7ea583086bf2b6b422b1d6b2a8
Jonsama <joni.suomi__@hotmail.com>
2014-10-24 15:14:02 +0300

    Päivitetty versio
    Tekijä: areyouwannadie-tiimi

--- ohjelma.py
+++ ohjelma.py
@@ -1,18 +1,21 @@
-a = 3
-b = 5
+a = int(input("Anna luku: "));
+b = int(input("Anna toinen luku: "));
 
 print("A:n arvo on", a, "ja B:n arvo on", b)
+while(1):
+	a = a * 3
+	b = b + 10
 
-a = a * 3
-b = b + 10
+	print("A:n arvo on", a, "ja B:n arvo on", b)
 
-print("A:n arvo on", a, "ja B:n arvo on", b)
-
-if a > b:
-	print("Game over")
-elif a > 1000:
-	print("Game over")
-elif b > 1000:
-	print("Game over")
-
+	if a > b:
+		print("Game over")
+		break
+	elif a > 1000:
+		print("Game over")
+		break
+	elif b > 1000:
+		print("Game over")
+		break
+	
 print("Heippa!")
